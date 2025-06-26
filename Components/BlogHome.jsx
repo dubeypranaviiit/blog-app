efe0e8a0b37fdfc4272ac28b534c1b7fe65c25ee
Pranav Abhimanyu <dpranav503@gmail.com>
2025-06-26 17:49:35 +0530

    change scroll bar

--- Components/BlogHome.jsx
+++ Components/BlogHome.jsx
@@ -27,13 +27,26 @@ const BlogHome = () => {
 
   return (
     <div>
-      <div className='flex justify-center gap-6 my-10'>
-        {['All', 'technology', 'Startup', 'lifestyle','other'].map((cat) => (
+      {/* <div className='flex justify-center gap-6 my-10'> */}
+        {/* {['All', 'technology', 'Startup', 'lifestyle','other'].map((cat) => (
           <button
             key={cat}
             onClick={() => setMenu(cat)}
             className={`py-1 px-4 rounded-sm ${
               menu === cat ? 'bg-black text-white' : 'bg-gray-200'
+            }`}
+          >
+            {cat}
+          </button>
+        ))}
+      </div> */}
+      <div className='flex justify-center md:justify-center gap-4 my-6 overflow-x-auto px-4 scrollbar-hide'>
+        {['All', 'technology', 'Startup', 'lifestyle', 'other'].map((cat) => (
+          <button
+            key={cat}
+            onClick={() => setMenu(cat)}
+            className={`py-2 px-4 text-sm whitespace-nowrap rounded-full transition ${
+              menu === cat ? 'bg-black text-white' : 'bg-gray-200 text-gray-800'
             }`}
           >
             {cat}
