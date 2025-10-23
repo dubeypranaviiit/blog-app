9d3ca441a66bf8e03855684cb381d1cca4a02e61
Pranav Abhimanyu <dpranav503@gmail.com>
2025-10-23 20:37:05 +0530

    completed

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  extends: ['next', 'next/core-web-vitals'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  rules: {},
+};
---
+++ app/cancel/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        Please try again later
+    </div>
+  )
+}
+
+export default page
